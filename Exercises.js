e354d19cf48160d9b2f116573ef9dfb0c26dc1d5
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-01-20 16:19:37 +0200

    5 answers
    finished

--- Exercises.js
+++ Exercises.js
@@ -1,35 +1,55 @@
 //Question 1
 function myReverse(str) {
-  // your code here
-  return "";
+  let out = "";
+      str= str.toString(10);
+        for (let i= str.length -1; i >=0; i--) {
+          out += str[i];
+        }
+    return out;             
 }
 
 //Question 2
 function allCombinations(str) {
   let combinations = [];
-  // your code here
-
-  //
+   for(i = 0; i < str.length; i++) {
+       for(j =i+1; j < str.length+1; j++) {
+        combinations.push(str.slice(i, j));
+       }
+   }
   return combinations;
 }
 
+
 //Question 3
 function allCaps(str) {
-  // your code here
-  return "";
+  
+  let array1 = str.split(' ');
+  let newarray1 = [];
+    
+  for(let i = 0; i < array1.length; i++){
+      newarray1.push(array1[i].charAt(0).toUpperCase()+array1[i].slice(1));
+  }
+  return newarray1.join(' ');
 }
+ 
 
 //Question 4
 function myPower(x, n) {
-  // your code here
-  return "";
+  let newNumber = Math.pow (x,n)
+  return newNumber;
 }
 
 //Question 5
 function getFirstNotRepeating(str) {
-  // your code here
-  return "";
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
+      return char;
+    }
+  }
+  return "_";
 }
+
 
 //Question 6 (Bonus)
 function isPrefectNumber(num) {
